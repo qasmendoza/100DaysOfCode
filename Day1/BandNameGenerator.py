a941ab0e267ccc05c89051ea8fa7aa1526d42b6b
Querriel Arvy Mendoza <querrielarvymendoza@ip-10-150-25-197.wireless.ksu.edu>
2024-03-18 16:21:44 -0500

    additing Day 1 assignment

---
+++ Day1/BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator") #Welcome note
+city = str(input("What is the name of the city you grew up in? "))
+pet = str(input("What is your pet's name? "))
+print("Your band name could be " + city + " " + pet)
