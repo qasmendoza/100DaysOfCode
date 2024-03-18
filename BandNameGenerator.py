43680b9f6d68b6f6d3c1e4d779d1b089a1c90edc
Querriel Arvy Mendoza <95595007+qasmendoza@users.noreply.github.com>
2024-03-18 16:19:23 -0500

    Delete BandNameGenerator.py

--- BandNameGenerator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band Name Generator") #Welcome note
-city = str(input("What is the name of the city you grew up in? "))
-pet = str(input("What is your pet's name? "))
-print("Your band name could be " + city + " " + pet)
